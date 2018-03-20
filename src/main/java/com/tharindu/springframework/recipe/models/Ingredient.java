7d1b50a242f2b89b5c881f86b64ff09249f04e5c
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2018-03-20 16:14:28 +0530

    Wiered relationship between Ingredient and UnitOfMeasure

--- src/main/java/com/tharindu/springframework/recipe/models/Ingredient.java
+++ src/main/java/com/tharindu/springframework/recipe/models/Ingredient.java
@@ -11,8 +11,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //todo
-    // private UnitOfMeasure uom;
+    @OneToOne
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -47,5 +47,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
