11f97380d3d9bc796387bd8ddb4043803fa7f5e6
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2018-03-20 15:22:02 +0530

    Created models for notes and recipe

---
+++ src/main/java/com/tharindu/springframework/recipe/models/Notes.java
@@ -0,0 +1,22 @@
+package com.tharindu.springframework.recipe.models;
+
+public class Notes {
+    private Recipe recipe;
+    private String recipeNotes;
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
