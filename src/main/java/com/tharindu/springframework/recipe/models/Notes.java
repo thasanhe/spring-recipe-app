436e55ce63d7520e12d501ee70c33418f62d4e0f
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2018-03-20 15:33:29 +0530

    Added notes to recipe relationship

--- src/main/java/com/tharindu/springframework/recipe/models/Notes.java
+++ src/main/java/com/tharindu/springframework/recipe/models/Notes.java
@@ -1,9 +1,6 @@
 package com.tharindu.springframework.recipe.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Notes {
@@ -12,7 +9,9 @@ public class Notes {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
     private Recipe recipe;
+
     private String recipeNotes;
 
     public Recipe getRecipe() {
